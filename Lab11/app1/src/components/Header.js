b3d1c9f454332038d52991fa17cae653712d3fb0
fnembaware <fadzai@tlu.ee>
2024-05-25 01:36:43 +0300

    added assets and component files

--- Lab11/app1/src/components/Header.js
+++ Lab11/app1/src/components/Header.js
@@ -1,6 +1,7 @@
 import {Link, NavLink} from 'react-router-dom'
 import Logo from '../assets/logo.png'
 export const Header = () => {
+    
   return (
     <header>
       <a href="/" className="logo">
